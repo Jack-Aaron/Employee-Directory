3345e1476da1436115f1d89ee0770563d78f0a40
Jonathan Alpart <panjialang@gmail.com>
2020-06-25 14:27:46 -0500

    img borders

--- src/components/Table-Body/index.js
+++ src/components/Table-Body/index.js
@@ -8,10 +8,17 @@ function TableBody(props) {
                 return (
                     <tr>
                         {/* <th scope="row"></th> */}
-                        <td><img src={result.picture.thumbnail} alt='' style={{ borderRadius: '15px' }} /></td>
+                        <td><img src={result.picture.thumbnail}
+                            alt=''
+                            style={{
+                                border: 'solid 2px black',
+                                borderRadius: '12px'
+                            }} /></td>
                         <td>{result.name.first + ' ' + result.name.last}</td>
-                        <td><a href={'tel:' + result.phone}>{result.phone}</a></td>
-                        <td><a href={'mailto:' + result.email}>{result.email}</a></td>
+                        <td><a href={'tel:' + result.phone}>
+                            {result.phone}</a></td>
+                        <td><a href={'mailto:' + result.email}>
+                            {result.email}</a></td>
                         <td>{moment(result.dob.date).format('MM/DD/YYYY')}</td>
                     </tr>
                 )
