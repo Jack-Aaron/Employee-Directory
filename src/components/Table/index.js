df6a145ac8a7da8fe863f27be1b4470b1337d6e2
Jonathan Alpart <panjialang@gmail.com>
2020-06-25 08:48:34 -0500

    react classes changed to react hooks

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import API from '../../utils/API';
 import Search from '../Search';
 import { Table } from 'reactstrap';
 import TableBody from '../Table-Body';
 import SortBtn from '../SortBtn';
 
-class EmployeeTable extends React.Component {
-    state = {
+
+function EmployeeTable() {
+
+    const [employees, setEmployees] = useState({
         results: [],
         filteredResults: [],
         currentSort: 'up'
-    };
+    });
 
-    // When the component mounts, load the next employee to be displayed
-    componentDidMount() { this.loadEmployees() }
+    useEffect(loadEmployees, []);
 
-    loadEmployees = () => {
+    function loadEmployees() {
         API.getEmployees()
             .then(res =>
-                this.setState({
+                setEmployees({
                     results: res.data.results,
                     filteredResults: res.data.results
                 })
             ).catch(err => console.log(err));
     };
 
-    searchFilter = (event) => {
-        this.setState({
-            filteredResults: this.state.results.filter(
+    const searchFilter = (event) => {
+        setEmployees({
+            ...employees,
+            filteredResults: employees.results.filter(
                 person => person.name.first.toLowerCase().includes(
                     event.target.value.toLowerCase())
                     || person.name.last.toLowerCase().includes(
@@ -36,49 +38,51 @@ class EmployeeTable extends React.Component {
         })
     };
 
-    render() {
-        return (
-            <div>
-                <Search
-                    onChange={this.searchFilter}
-                    placeholder='John Smith'
+    const sortFilter = () => {
+        if (employees.currentSort === 'up') {
+            setEmployees({
+                ...employees,
+                filteredResults: employees.filteredResults.sort(
+                    (a, b) => a.name.last.localeCompare(b.name.last)),
+                currentSort: 'down'
+            })
+        }
+        else {
+            setEmployees({
+                ...employees,
+                filteredResults: employees.filteredResults.sort(
+                    (a, b) => b.name.last.localeCompare(a.name.last)),
+                currentSort: 'up'
+            })
+        }
+    };
+
+    return (
+        <div>
+            <Search
+                onChange={searchFilter}
+                placeholder='John Smith'
+            />
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>
+                            Name
+                        <SortBtn onClick={sortFilter} />
+                        </th>
+                        <th>Phone Number</th>
+                        <th>Email</th>
+                        <th>DOB</th>
+                    </tr>
+                </thead>
+                <TableBody
+                    results={employees.filteredResults}
                 />
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Image</th>
-                            <th>
-                                Name
-                        <SortBtn onClick={() => {
-                                    if (this.state.currentSort === 'up') {
-                                        this.setState({
-                                            filteredResults: this.state.filteredResults.sort(
-                                                (a, b) => a.name.last.localeCompare(b.name.last)),
-                                            currentSort: 'down'
-                                        })
-                                    }
-                                    else {
-                                        this.setState({
-                                            filteredResults: this.state.filteredResults.sort(
-                                                (a, b) => b.name.last.localeCompare(a.name.last)),
-                                            currentSort: 'up'
-                                        })
-                                    }
-                                }
-                                } />
-                            </th>
-                            <th>Phone Number</th>
-                            <th>Email</th>
-                            <th>DOB</th>
-                        </tr>
-                    </thead>
-                    <TableBody
-                        results={this.state.filteredResults}
-                    />
-                </Table>
-            </div>
-        );
-    }
+            </Table>
+        </div>
+    );
 }
+
 
 export default EmployeeTable;
