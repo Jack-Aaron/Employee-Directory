f27e5270e75ea5ee607fb25479b81e5c9b922e28
Jonathan Alpart <panjialang@gmail.com>
2020-06-26 08:20:46 -0500

    Updates

---
+++ precache-manifest.bd19af07126bed9229078fdf1a6eb536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b88ecbe255278ba6dc663550f11398",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "2287afb62a5fadbc7e42",
+    "url": "/employee-directory/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "20c3c69275d050efc0f3",
+    "url": "/employee-directory/static/css/main.e4ffa722.chunk.css"
+  },
+  {
+    "revision": "2287afb62a5fadbc7e42",
+    "url": "/employee-directory/static/js/2.c38fc6b6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-directory/static/js/2.c38fc6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c3c69275d050efc0f3",
+    "url": "/employee-directory/static/js/main.1d24074f.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
